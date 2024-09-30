784aecc05d2a36b1128f243a15f3fd840166796c
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-09-30 22:06:56 +0000

    comparison

---
+++ basics/04_comaprisions.js
@@ -0,0 +1,4 @@
+console.log(null==0)
+console.log(null>0)
+console.log(null>=0) //       equity check (==) and comaparisons >,< and >= works differently  comparision converts null to 0 and gives true
+// whereas == doesnt convert
