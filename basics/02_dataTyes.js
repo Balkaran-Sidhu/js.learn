8a5c7b0d5b3314bf4d6cf9c728ae666451e48d10
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-09-30 21:27:45 +0000

    datatypes

---
+++ basics/02_dataTyes.js
@@ -0,0 +1,10 @@
+"use strict"; // all the code will be treated as new version acoording to ECMA
+let age // undefined
+
+// null=> standalone value... means its empty no number nothing    .... its type is object
+//undefined => means no value is assigned.....  its type is undefined because undefined is its self a datatype
+// symbol => for the unquieness 
+//bigint => for bigger ints like stocks
+
+// object
+
