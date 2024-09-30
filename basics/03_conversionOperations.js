3864b357bf4631aaf6b4822f1cd9ac7c6b098e6b
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-09-30 21:56:43 +0000

    conversions

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = 33
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+console.log(typeof(valueInNumber));
+
+let str = String (score)
+console.log(typeof(str))
+/*  33 => 33
+    "33abc" => type can be converted but it will be NAN (not a number)
+
+    when coverting to boolean when 1 => true and 0 => false and evrything else is converted to true
+
+*/
+
+// ...................  Operations .....................
+let num = 7
+let negnum = -num // will convert to negative to num
+
