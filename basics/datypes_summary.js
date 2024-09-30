73698ddf0f60f3fdf3703d87de72cc9027f3d0ce
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-09-30 22:42:50 +0000

    memory

--- basics/datypes_summary.js
+++ basics/datypes_summary.js
@@ -16,9 +16,27 @@ let obj ={
 }
 
 // function
-
 const fun = function(){
-    console.log("Hello World");
-    
+    console.log("Hello World");}
+
+    // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// .............................................Memory
+
+// Stack (in Primitive datatypesstcak memory is used ), Heap (in non primitive datatypes heap memory is used)
+
+let user1 = {
+    email: "user@cc.com",
+    id: 123
 }
-fun
+
+let user2 = user1
+
+user2.email = "user2@cc.com"
+
+console.log(user1.email)    
+console.log(user2.email);   //  email is changed as referece was giben of the heap memory and the the ture data in that memory was changed 
+// so the change was also made for the user 1 they both direct to same heap in the memory.
+
+
