55b04c3ec6bddb753edce4a88c478da7c3011232
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-10-17 03:29:59 +0000

    ifelse

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+//if
+
+// const score = 200
+
+// if(score>100){
+//     const power ="fly"
+//     console.log(`user power: ${power}`)
+// }
+
+// const balance = 1000
+
+// if(balance>500) console.log("test");  // implicit scope
+
