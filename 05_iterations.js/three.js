c7b7861d5d4688bd3c2a55a5f96a60221221807c
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-10-21 17:45:22 +0000

    three

---
+++ 05_iterations.js/three.js
@@ -0,0 +1,12 @@
+const coding = ["js", "java", "python", "cpp", "ruby"]
+
+coding.forEach( function(item){
+    //console.log(item)
+} )
+
+coding.forEach((item)=>{
+    //console.log(item)
+} )
+
+// function printme(item){
+//     console.log(item);}
