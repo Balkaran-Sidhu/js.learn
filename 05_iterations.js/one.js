993594da0a89f6413c393014f08dd790baa6c870
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-10-21 17:45:14 +0000

    one

---
+++ 05_iterations.js/one.js
@@ -0,0 +1,37 @@
+// for loop
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+// }
+
+
+// break and continue
+
+//for of
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+console.log("Hello")
+// const map = new Map()
+
+// map.set('IN', "India")
+// map.set('Fr', "France")
+// map.set('Usa', "United stated of america")
+
+
+// for (const [key,value] of map) {
+//    // console.log(key, '-', value)
+// }
+
+// const myobject = {
+//     'game1' : 'nsf',
+//     'game2' : 'mario'
+// }
+
+// // objects not iterable by for of loop
+// for (const key of myobject) {
+//     // console.log(key, '-', value)
+//  }
