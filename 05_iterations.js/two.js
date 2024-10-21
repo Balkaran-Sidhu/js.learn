435aef1a550ed285f810bcaa1e24c9cd1e7a52ae
Balkaran-Sidhu <104658356+Balkaran-Sidhu@users.noreply.github.com>
2024-10-21 17:45:30 +0000

    two

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,12 @@
+const myobject = {
+    game1 : 'nsf',
+    game2 : 'mario',
+    game3 : 'contra'
+}
+
+//for in loop works for object
+
+for (const key in myobject) {
+    //console.log(myobject[key])
+}
+
